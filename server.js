c7bd4ee002448c3f503ea254ef0ea55d77dce1f4
vinod0031 <kvinodkumar0031@gmail.com>
2024-04-16 22:00:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const MONGODB_URI = 'mongodb://localhost:27017';//setup MongoDB your device 
+mongoose.connect(MONGODB_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error(err));
+
+
+const ItemSchema = new mongoose.Schema({
+images: String,
+mrp: String,
+discount:String
+});
+
+const Item = mongoose.model('Item', ItemSchema);
+
+
+app.get('/api/items', async (req, res) => {
+try {
+const items = await Item.find();
+res.json(items);
+} catch (err) {
+console.error(err);
+res.status(500).json({ message: 'Server Error' });
+}
+});
+
+const WomenSchema = new mongoose.Schema({
+    images: String,
+    mrp: String,
+    discount:String
+    });
+
+const Women = mongoose.model('Women', WomenSchema);
+
+app.get('/api/Womens', async (req, res) => {
+    try {
+    const items = await Women.find();
+    res.json(items);
+    } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+    }
+    });
+
+
+const KidsSchema = new mongoose.Schema({
+    images: String,
+    mrp: String,
+    discount:String
+    });
+
+const Kids = mongoose.model('Kids', KidsSchema);
+
+app.get('/api/kids', async (req, res) => {
+    try {
+    const items = await Kids.find();
+    res.json(items);
+    } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+    }
+    });
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
